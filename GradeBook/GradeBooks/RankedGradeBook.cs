ad52499eee3d6f40274975973e8420d76bac76ab
dawid-krupinski <dawid.krupinski1@gmail.com>
2018-02-17 20:45:41 +0100

    Third task

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,43 @@ namespace GradeBook.GradeBooks
         {
             this.Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                int betterStudentsCount = 0;
+
+                foreach (var student in Students)
+                {
+                    if (student.AverageGrade > averageGrade)
+                    {
+                        betterStudentsCount++;
+                    }
+                }
+
+                var studentPlace = (double)(betterStudentsCount + 1) / Students.Count;
+
+                if (studentPlace <= 0.2)
+                {
+                    return 'A';
+                } else if (studentPlace <= 0.4)
+                {
+                    return 'B';
+                } else if (studentPlace <= 0.6)
+                {
+                    return 'C';
+                } else if (studentPlace <= 0.8)
+                {
+                    return 'D';
+                }
+            }
+
+            return 'F';
+        }
     }
 }
